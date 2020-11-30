5ef693a671cda538730efdfe8f04bf279c5babc5
fanidc7 <fanidc7@gmail.com>
2020-11-30 15:44:47 +0700

    db edit tagihan

--- db_hipam.sql
+++ db_hipam.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Nov 2020 pada 10.17
--- Versi server: 10.4.13-MariaDB
--- Versi PHP: 7.4.8
+-- Generation Time: Nov 30, 2020 at 09:08 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `failed_jobs`
+-- Table structure for table `failed_jobs`
 --
 
 CREATE TABLE `failed_jobs` (
@@ -39,7 +39,7 @@ CREATE TABLE `failed_jobs` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `migrations`
+-- Table structure for table `migrations`
 --
 
 CREATE TABLE `migrations` (
@@ -49,7 +49,7 @@ CREATE TABLE `migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `migrations`
+-- Dumping data for table `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
@@ -69,7 +69,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `password_resets`
+-- Table structure for table `password_resets`
 --
 
 CREATE TABLE `password_resets` (
@@ -81,7 +81,7 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `retribusis`
+-- Table structure for table `retribusis`
 --
 
 CREATE TABLE `retribusis` (
@@ -100,16 +100,17 @@ CREATE TABLE `retribusis` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `retribusis`
+-- Dumping data for table `retribusis`
 --
 
 INSERT INTO `retribusis` (`id`, `retribusi_name`, `tarif1`, `tarif2`, `abonemen`, `kompensasi`, `user_entry`, `user_update`, `user_delete`, `deleted_at`, `created_at`, `updated_at`) VALUES
-(1, 'Default', 0, 0, 0, 0, 'Fani7', 'Fani7', NULL, NULL, '2020-11-26 01:45:44', '2020-11-25 23:44:11');
+(1, 'Default', 0, 0, 0, 0, 'Fani7', 'Fani7', NULL, NULL, '2020-11-26 01:45:44', '2020-11-25 23:44:11'),
+(5, 'Retribusi 1', 235, 34346, 545, 235435, 'Fani7', NULL, NULL, NULL, '2020-11-29 21:04:17', '2020-11-29 21:04:17');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `rts`
+-- Table structure for table `rts`
 --
 
 CREATE TABLE `rts` (
@@ -124,16 +125,17 @@ CREATE TABLE `rts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `rts`
+-- Dumping data for table `rts`
 --
 
 INSERT INTO `rts` (`id_rt`, `id_rw`, `user_entry`, `user_update`, `user_delete`, `deleted_at`, `created_at`, `updated_at`) VALUES
-(0, 0, 'Fani7', NULL, 'Fani7', NULL, '2020-11-28 00:51:56', '2020-11-28 01:35:57');
+(0, 0, 'Fani7', NULL, 'Fani7', NULL, '2020-11-28 00:51:56', '2020-11-28 01:35:57'),
+(3, 2, 'Fani7', NULL, NULL, NULL, '2020-11-29 21:04:53', '2020-11-29 21:04:53');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `rws`
+-- Table structure for table `rws`
 --
 
 CREATE TABLE `rws` (
@@ -148,16 +150,51 @@ CREATE TABLE `rws` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `rws`
+-- Dumping data for table `rws`
 --
 
 INSERT INTO `rws` (`id_rw`, `id_retribusi`, `user_entry`, `user_update`, `user_delete`, `deleted_at`, `created_at`, `updated_at`) VALUES
-(0, 1, 'Fani7', NULL, NULL, NULL, '2020-11-27 03:45:34', '2020-11-27 03:45:34');
+(0, 1, 'Fani7', NULL, NULL, NULL, '2020-11-27 03:45:34', '2020-11-27 03:45:34'),
+(2, 5, 'Fani7', NULL, NULL, NULL, '2020-11-29 21:04:39', '2020-11-29 21:04:39');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `tagihans`
+--
+
+CREATE TABLE `tagihans` (
+  `id_tagihan` bigint(20) UNSIGNED NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `bulan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tahun` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `metersekarang` int(11) NOT NULL,
+  `metersebelumnya` int(11) NOT NULL,
+  `pakai` int(11) NOT NULL,
+  `tarif1` int(11) NOT NULL,
+  `totaltarif1` int(11) NOT NULL,
+  `tarif2` int(11) NOT NULL,
+  `totaltarif2` int(11) NOT NULL,
+  `abonemen` int(11) NOT NULL,
+  `totalabonemen` int(11) NOT NULL,
+  `kompensasi` int(11) NOT NULL,
+  `totalkompensasi` int(11) NOT NULL,
+  `rekeningpakaiair` int(11) NOT NULL,
+  `denda` int(11) NOT NULL,
+  `totaltagihan` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `user_entry` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_update` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_delete` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -179,86 +216,99 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `name`, `password`, `passwordnohash`, `id_rt`, `id_rw`, `roles`, `user_entry`, `user_update`, `user_delete`, `remember_token`, `deleted_at`, `created_at`, `updated_at`) VALUES
-(1, 'Fani7', 'Fani Dwi Cahyo', '$2y$10$YlzFxLqD6i9UxfWmIZCJmOl/7V0prs0yeIW3eR02V/AxelM8jrfdi', 'fanidwicahyo', 0, 0, 'ADMIN', 'Fani', NULL, NULL, NULL, NULL, '2020-11-25 18:52:28', '2020-11-25 18:52:28');
+(1, 'Fani7', 'Fani Dwi Cahyo', '$2y$10$YlzFxLqD6i9UxfWmIZCJmOl/7V0prs0yeIW3eR02V/AxelM8jrfdi', 'fanidwicahyo', 0, 0, 'ADMIN', 'Fani', NULL, NULL, NULL, NULL, '2020-11-25 18:52:28', '2020-11-25 18:52:28'),
+(3, 'Aldi12', 'Aldi Putra', '$2y$10$GjDLy9HR8OGUF/gfkm15OOrV0XKxdcvbBXTamAs0/HcjHiDqUUm0u', 'qwertyuiop', 3, 2, 'PELANGGAN', 'Fani7', NULL, NULL, NULL, NULL, '2020-11-29 21:05:14', '2020-11-29 21:05:14');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `failed_jobs`
+-- Indexes for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `migrations`
+-- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `password_resets`
+-- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
--- Indeks untuk tabel `retribusis`
+-- Indexes for table `retribusis`
 --
 ALTER TABLE `retribusis`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `rts`
+-- Indexes for table `rts`
 --
 ALTER TABLE `rts`
   ADD PRIMARY KEY (`id_rt`,`id_rw`);
 
 --
--- Indeks untuk tabel `rws`
+-- Indexes for table `rws`
 --
 ALTER TABLE `rws`
   ADD PRIMARY KEY (`id_rw`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `tagihans`
+--
+ALTER TABLE `tagihans`
+  ADD PRIMARY KEY (`id_tagihan`,`id_user`,`bulan`,`tahun`);
+
+--
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_username_unique` (`username`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `failed_jobs`
+-- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `migrations`
+-- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT untuk tabel `retribusis`
+-- AUTO_INCREMENT for table `retribusis`
 --
 ALTER TABLE `retribusis`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `tagihans`
+--
+ALTER TABLE `tagihans`
+  MODIFY `id_tagihan` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
